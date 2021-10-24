d3f23a478f5debca0650c6f580a6d0646250dea8
Dragutin Marjanović <dmarjanovic94@gmail.com>
2021-10-24 15:27:51 +0200

    Use property based tests

--- src/test/scala/com/github/drmarjanovic/app/api/model/ArticleResponseDataSpec.scala
+++ src/test/scala/com/github/drmarjanovic/app/api/model/ArticleResponseDataSpec.scala
@@ -4,35 +4,26 @@ import com.github.drmarjanovic.app.domain.{Article, ArticleId}
 import zio.test.Assertion._
 import zio.test._
 
-import java.time.LocalDateTime
-
 object ArticleResponseDataSpec extends DefaultRunnableSpec {
   def spec: ZSpec[Environment, Failure] = suite("ArticleResponseDataSpec")(
-    test("fromDomain correctly transforms domain to response data") {
-      val now = LocalDateTime.now
-      val article = Article(
-        id = ArticleId(1),
-        slug = "test-slug",
-        title = "test-title",
-        description = "test-description",
-        body = "test-body",
-        createdAt = now,
-        updatedAt = now
-      )
+    testM("fromDomain correctly transforms domain to response data") {
+      check(Gen.anyString, Gen.anyString, Gen.anyString, Gen.anyString, Gen.anyLocalDateTime, Gen.anyLocalDateTime) {
+        (slug, title, description, body, createdAt, updatedAt) =>
+          val article = Article(ArticleId(1), slug, title, description, body, createdAt, updatedAt)
+          val result  = ArticleResponseData.fromDomain(article)
 
-      val result = ArticleResponseData.fromDomain(article)
+          val expected = ArticleResponseData(
+            title = title,
+            slug = slug,
+            body = body,
+            description = description,
+            tags = Set.empty,
+            createdAt = createdAt,
+            updatedAt = updatedAt
+          )
 
-      val expected = ArticleResponseData(
-        title = "test-title",
-        slug = "test-slug",
-        body = "test-body",
-        description = "test-description",
-        tags = Set.empty,
-        createdAt = now,
-        updatedAt = now
-      )
-
-      assert(result)(equalTo(expected))
+          assert(result)(equalTo(expected))
+      }
     }
   )
 }
