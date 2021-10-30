13cbbb6feb19b37252e508d35179406c8fa9102c
Dragutin Marjanović <dmarjanovic94@gmail.com>
2021-10-30 19:34:33 +0200

    Improve getting param from query

--- src/main/scala/com/github/drmarjanovic/app/api/routes/Articles.scala
+++ src/main/scala/com/github/drmarjanovic/app/api/routes/Articles.scala
@@ -10,8 +10,8 @@ object Articles {
   val routes: Http[Has[ArticlesRepo], Throwable, Request, UResponse] =
     Http.collectM[Request] {
       case req @ Method.GET -> Root / "articles" =>
-        val limit  = req.url.queryParams.get("limit").flatMap(_.headOption).map(_.toInt).getOrElse(20)
-        val offset = req.url.queryParams.get("offset").flatMap(_.headOption).map(_.toInt).getOrElse(0)
+        val limit  = getQueryParam(req, "limit").getOrElse(20)
+        val offset = getQueryParam(req, "offset").getOrElse(0)
 
         ArticlesRepo(_.all(limit, offset))
           .map(data => ArticlesResponse.fromDomain(data).toJson)
--- src/main/scala/com/github/drmarjanovic/app/api/routes/package.scala
+++ src/main/scala/com/github/drmarjanovic/app/api/routes/package.scala
@@ -1,9 +1,15 @@
 package com.github.drmarjanovic.app.api
 
-import zhttp.http.{HTTP_CHARSET, Header, HttpData, Response, Status, UResponse}
+import zhttp.http._
 import zio.Chunk
 
+import scala.util.Try
+
 package object routes {
+
+  def getQueryParam[A](req: Request, key: String): Option[A] =
+    req.url.queryParams.get(key).flatMap(_.headOption).flatMap(value => Try(value.asInstanceOf[A]).toOption)
+
   def unprocessableEntity(data: String): UResponse = Response.http(
     Status.UNPROCESSABLE_ENTITY,
     content = HttpData.CompleteData(Chunk.fromArray(data.getBytes(HTTP_CHARSET))),
