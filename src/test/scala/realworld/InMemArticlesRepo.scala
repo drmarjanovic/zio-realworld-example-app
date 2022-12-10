b284698cf955a6c4c85e544ffe4e9b237ced1358
Dragutin Marjanović <dmarjanovic94@gmail.com>
2022-12-10 19:43:11 +0100

    Fix code remarks

--- src/test/scala/realworld/InMemArticlesRepo.scala
+++ src/test/scala/realworld/InMemArticlesRepo.scala
@@ -1,31 +1,30 @@
 package realworld
-import zio.{Task, ULayer, ZIO, ZLayer}
+import zio._
 
-import java.time.LocalDateTime
-import scala.collection.mutable
+final class InMemArticlesRepo extends ArticlesRepo {
 
-final class InMemArticlesRepo private (init: mutable.Seq[Article] = mutable.Seq.empty[Article]) extends ArticlesRepo {
-
-  private var articles = init
+  private var articles = List.empty[Article]
 
   def insert(title: String, body: String, description: String): Task[Article] =
-    ZIO.succeed {
-      val article = Article(
-        id = ArticleId(1),
-        title = title,
-        slug = title,
-        body = body,
-        description = description,
-        createdAt = LocalDateTime.now(),
-        updatedAt = LocalDateTime.now()
-      )
-
+    for {
+      id  <- Random.nextIntBounded(1000)
+      now <- Clock.localDateTime
+      article = Article(
+                  id = ArticleId(id),
+                  title = title,
+                  slug = title,
+                  body = body,
+                  description = description,
+                  createdAt = now,
+                  updatedAt = now
+                )
+    } yield {
       articles = articles :+ article
       article
     }
 
   def fetchAll(limit: Int, offset: Int): Task[List[Article]] =
-    ZIO.succeed(articles.slice(offset, offset + limit).toList)
+    ZIO.succeed(articles.slice(offset, offset + limit))
 
   def findBySlug(slug: String): Task[Option[Article]] =
     ZIO.succeed(articles.find(_.slug == slug))
